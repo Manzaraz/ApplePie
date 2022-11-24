64f59a014d1b318fa202565deab2123562e34bbb
Christian Manzaraz <magnusmanz@gmail.com>
2022-11-24 12:04:12 -0300

    Second Part: Beginning a game: Defining words and turns Defining number of Wins and Losses

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -7,20 +7,27 @@
 
 import UIKit
 
+var listOfWords = [ "corsario", "swift", "glorioso","incandescente", "pestaña", "insecto", "programa"] // Lista de palabras a adivinar
+let incorrectMovesAllowed = 7 // Establece cuántas veces está permitido equivocarse por ronda. Cuanto menor sea el número, más difícil será que el jugador gane
+var totalWins = 0
+var totalLosses = 0
+
+
 class ViewController: UIViewController {
     @IBOutlet var treeImageView: UIImageView!
     @IBOutlet var correctWordLabel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
     
+    
     @IBOutlet var letterButtons: [UIButton]!
-    
-    
-    
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+ 
     }
+    
+
 
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
